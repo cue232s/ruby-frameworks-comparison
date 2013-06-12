47e59d0b4f4545bc8e4177c89ae9c48d7cd8ee0f
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2013-06-12 12:16:25 +0300

    rails4

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0.rc1'
+gem 'slim'
+gem 'sequel'
+gem 'mysql2'
+gem 'thin'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyProject::Application.config.secret_key_base = 'a99741c698a7b0ea7d5aa23483213997eae87cb3ddc541e1f054bf276312674d7a82493ac7282c4ab28ec51066d95cac37b4a68be092551f3d9d8a6b9f707ba3'
